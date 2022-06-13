e692b686fa982cc094e85739e87ddb4d74d1b8c9
htmldav <htmldav@mail.ru>
2022-06-13 14:34:21 +0300

    try1

---
+++ config.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.18.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-09d56f8956ab235b3" # us-west-2
+  instance_type = "t2.micro"
+}
